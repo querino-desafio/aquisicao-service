dbcf220d87e7b686a2df60bf3ad6392ab395aac4
Dorival Querino da Silva <dorival.querino@gmail.com>
2023-02-27 23:12:12 -0300

    first commit

---
+++ src/main/java/com/silva/pospagamento/aquisicaoservice/AquisicaoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.silva.pospagamento.aquisicaoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquisicaoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquisicaoServiceApplication.class, args);
+	}
+
+}
